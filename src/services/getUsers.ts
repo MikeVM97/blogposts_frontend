1129d1bb03eb0b95841ee4ab0c1e7cd9a7cf13cd
Mike <vivancomayta@gmail.com>
2023-09-07 23:20:05 -0500

    env variable added

--- src/services/getUsers.ts
+++ src/services/getUsers.ts
@@ -1,8 +1,12 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const URL = import.meta.env.MODE === "production"
+  ? "https://blogposts.up.railway.app"
+  : "http://localhost:3000";
 
 export const usersApi = createApi({
   reducerPath: 'usersApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: URL }),
   endpoints: (builder) => ({
     getUsers: builder.query<User[], void>({
       query: () => '/users',
