3e72331e680647681018f7c26dc28c2234d7e5bd
Mike <vivancomayta@gmail.com>
2023-09-18 20:38:50 -0500

    data fetching with effect & loader added

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useGetUsersQuery } from "../services/getUsers";
+// import { useGetUsersQuery } from "../services/getUsers";
 import { useEffect, useState } from "react";
 import { useAppSelector, useAppDispatch } from "../app/hook";
 import { RootState } from "../app/store";
@@ -19,53 +19,41 @@ import { uploadImage } from "../app/firebase";
 export default function HomePage() {
   const user = useAppSelector((state: RootState) => state.user);
   const dispatch = useAppDispatch();
-  const { data: users, error, isLoading } = useGetUsersQuery();
+  // const { data: users, error, isLoading } = useGetUsersQuery();
+  const [users, setUsers] = useState([]);
   const [postsOrdered, setPostsOrdered] = useState<Post[]>([]);
-  const [isHidden, setIsHidden] = useState(true);
-  const [text, setText] = useState('PARA COMENZAR A CREAR POSTS');
+  const [loader, setLoader] = useState(false);
 
   const navigate = useNavigate();
 
-  const URL = process.env.NODE_ENV === "production"
-  ? "https://blogposts.up.railway.app"
-  : "http://localhost:3000";
+  const URL =
+    process.env.NODE_ENV === "production"
+      ? "https://blogposts.up.railway.app"
+      : "http://localhost:3000";
 
   useEffect(() => {
-    fetch(`${URL}/api/auth/verify`, {
-      method: 'GET',
-      credentials: 'include',
-    })
-    .then(res => res.json())
-    .then(data => {
-      if (!data.message) {
-        dispatch(setLogged(true));
-        console.log("Logged in");
-      } else {
-        dispatch(setLogged(false));
-        console.log(data.message);
-      }
-    })
-    .catch(err => console.error(err));
-  }, [dispatch, URL]);
-
-  useEffect(() => {
-    if (users) {
-      const posts = users
-        .map((user) => user.posts)
-        .flat()
-        .sort((a, b) => Number(b.postId) - Number(a.postId));
+    fetch(`${URL}/users`)
+    .then((res) => res.json())
+    .then((data) => {
+      setUsers(data);
+      const posts = data
+      .map((user: User) => user.posts)
+      .flat()
+      .sort((a: Post, b: Post) => Number(b.postId) - Number(a.postId));
       setPostsOrdered(posts);
-    }
-  }, [users]);
+    })
+    .catch((err) => console.error(err));
+  }, [URL]);
 
   async function handleLogout() {
+    setLoader(true);
     try {
       const logoutRequest = await fetch(`${URL}/api/auth/logout`, {
-        method: 'POST',
-        credentials: 'include',
+        method: "GET",
+        credentials: "include",
       });
-
       if (logoutRequest.ok) {
+        setLoader(false);
         dispatch(setEmail(""));
         dispatch(setId(""));
         dispatch(setIsVerified(false));
@@ -74,8 +62,9 @@ export default function HomePage() {
         dispatch(setPhotoUrl("blank"));
         dispatch(setPosts([]));
         dispatch(setUsername(""));
-        navigate(0);
+        // navigate(0);
       } else {
+        setLoader(false);
         console.log("Error al cerrar sesión.");
       }
     } catch (error) {
@@ -87,16 +76,19 @@ export default function HomePage() {
     const file: FileList | null = e.target.files;
     if (file !== null) {
       const urlImage: string = await uploadImage(file[0], user.username);
-      const sendData = await fetch(`${URL}/api/user/updateprofileimage/${user.id}`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ newPhotoUrl: urlImage }),
-      });
+      const sendData = await fetch(
+        `${URL}/api/user/updateprofileimage/${user.id}`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ newPhotoUrl: urlImage }),
+        }
+      );
       if (sendData.ok) {
-        const { newPhotoUrl, newPosts } =  await sendData.json();
+        const { newPhotoUrl, newPosts } = await sendData.json();
         if (newPhotoUrl) {
           dispatch(setPhotoUrl(newPhotoUrl));
         }
@@ -116,29 +108,6 @@ export default function HomePage() {
   function goToCreatePost() {
     if (user.isVerified) {
       navigate("/post");
-    } else {
-      setIsHidden(false);
-    }
-  }
-
-  async function resendEmailVerification() {
-    try {
-      const sendVerification = await fetch(`${URL}/api/user/resendemail/${user.id}`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email: user.email }),
-      });
-
-      if (sendVerification.ok) {
-        alert("El email de verificación ha sido envíado.");
-      } else {
-        console.log("Error al enviar el email de verificación.");
-      }
-    } catch (error) {
-      console.log(error)
     }
   }
 
@@ -161,7 +130,7 @@ export default function HomePage() {
 
   if (user.logged) {
     content = (
-      <header className="flex justify-between items-center p-4 border-2 border-blue-700">
+      <header className="flex justify-between items-center p-4 border-b-2 border-blue-700">
         <label htmlFor="input-file" className="cursor-pointer">
           <div
             style={profileStyle}
@@ -190,7 +159,7 @@ export default function HomePage() {
     );
   } else {
     content = (
-      <header className="flex justify-between items-center py-4 px-8">
+      <header className="flex justify-between items-center py-4 px-8 border-b-2 border-blue-700">
         <Link
           className="border border-zinc-700 py-1 px-2 rounded-lg bg-white hover:text-white hover:bg-zinc-700 hover:border-white text-xl"
           to="/login"
@@ -207,52 +176,34 @@ export default function HomePage() {
     );
   }
 
-  if (isLoading) {
+  /* if (isLoading) {
     posts = <div>Loading...</div>;
   }
 
   if (error) {
     posts = <p>Oh no, un error ah ocurrido</p>;
-  }
+  } */
 
   if (!users) {
     posts = <p>Users not found</p>;
   }
 
   if (users) {
-    posts = <Posts posts={postsOrdered} users={users} user={user} setIsHidden={setIsHidden} setText={setText} />;
+    posts = <Posts posts={postsOrdered} users={users} user={user} />;
   }
 
   return (
     <div className={`relative bg-gray-300`}>
-      {content}
-      <h1 className="border-y-2 border-blue-700 text-center p-4 text-5xl">
-        BlogPosts
-      </h1>
-      {posts}
-      <div className={`${!isHidden ? 'block' : 'hidden'} absolute top-0 left-0 w-full h-full bg-gray-300/60`}>
-      </div>
-      <div className={`fixed top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 border-2 border-orange-950 rounded-xl p-4 text-center bg-slate-200 ${!isHidden ? 'block' : 'hidden'}`}>
-        <p className="p-2">
-          {text} PRIMERO DEBE VERIFICAR SU CUENTA.
-        </p>
-        <p className="p-2">
-          HAGA CLICK AQUÍ PARA ENVÍAR UN MENSAJE DE VERIFICACIÓN A SU CORREO ELECTRÓNICO:
-        </p>
-        <button 
-          className="border-2 border-orange-900 px-2 py-1 rounded-lg bg-white hover:bg-orange-900 hover:text-white"
-          onClick={resendEmailVerification}
-        >
-          Enviar
-        </button>
-        <button
-          className="absolute top-2 right-2 rounded-full border-2 border-orange-900 bg-white hover:bg-orange-900 hover:text-white w-7 h-7"
-          onClick={() => setIsHidden(true)}
-        >
-          <span className="absolute top-[37%] left-[52%] -translate-y-2/4 -translate-x-2/4 text-xl">
-            ×
-          </span>
-        </button>
+      <section className={`${loader ? 'hidden' : 'block'}`}>
+        {content}
+        <h1 className="border-b-2 border-blue-700 text-center p-4 text-5xl">
+          BlogPosts
+        </h1>
+        {posts}
+      </section>
+      <div className={`${loader ? 'block' : 'hidden'} absolute top-0 left-0 w-screen h-screen p-4 bg-gray-200 flex flex-col justify-center items-center`}>
+        <h3 className="text-xl font-bold font-mono">Saliendo...</h3>
+        <div className="spinner"></div>
       </div>
     </div>
   );
