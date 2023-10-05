57c9df953b3cd8ffd7901c6f7d4a4d58e373e626
Mike <vivancomayta@gmail.com>
2023-10-05 18:19:51 -0500

    present updated

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   /* testEnvironment provided by jest-environment-jsdom, prevents the Error: */
   /* Test environment jest-environment-jsdom cannot be found. Make sure the testEnvironment configuration option points to an existing node module. */
   testEnvironment: 'jsdom',
