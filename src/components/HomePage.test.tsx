ce2220aff1dfedde66b56d26032a608efc8ced1f
Mike <vivancomayta@gmail.com>
2023-10-26 17:54:47 -0500

    migratin to vitest

--- src/components/HomePage.test.tsx
+++ src/components/HomePage.test.tsx
@@ -1,12 +1,14 @@
-import "@testing-library/jest-dom";
+// import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { describe, it, expect, beforeEach } from "vitest";
+import HomePage from "./HomePage";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { persistor, store } from "../app/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "../app/store.ts";
-import HomePage from "./HomePage";
 
-beforeEach(() =>
+beforeEach(() => {
   render(
     <BrowserRouter>
       <Provider store={store}>
@@ -15,12 +17,13 @@ beforeEach(() =>
         </PersistGate>
       </Provider>
     </BrowserRouter>
-  )
-);
+  );
+});
 
-describe("App Component", () => {
-  it("It should contains the word BlogPosts", () => {
-    expect(screen.queryByText("BlogPosts")).toBeInTheDocument();
+describe("App Component works", () => {
+  it("some test 1", () => {
+    const element = screen.queryByText("BlogPosts");
+    expect(element).toBeInTheDocument();
   });
 });
 
