682378f238c519ef83bebf23e77cc292b342c0ea
MikeVM97 <vivancomayta@gmail.com>
2023-11-01 16:15:45 -0500

    name dir changed

---
+++ src/services/uploadImage.ts
@@ -0,0 +1,11 @@
+import { app } from "services/firebase"
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage"
+
+export const storage = getStorage(app)
+
+export async function uploadImage(file: File, alias: string) {
+	const storageRef = ref(storage, alias)
+	await uploadBytes(storageRef, file)
+	const url = await getDownloadURL(storageRef)
+	return url
+}
