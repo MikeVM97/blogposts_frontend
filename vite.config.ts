aa6d144615d6577d8a236e093406af0143be9701
MikeVM97 <vivancomayta@gmail.com>
2023-11-01 14:37:10 -0500

    migrating from jest to vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { fileURLToPath, URL } from "node:url"
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom"
-  }
+	plugins: [react()],
+	test: {
+		environment: "jsdom"
+	},
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./", import.meta.url)),
+			pages: fileURLToPath(new URL("./src/pages/", import.meta.url)),
+			components: fileURLToPath(new URL("./src/components/", import.meta.url)),
+			schemas: fileURLToPath(new URL("./src/schemas/", import.meta.url)),
+			api: fileURLToPath(new URL("./src/api/", import.meta.url)),
+			reducers: fileURLToPath(new URL("./src/reducers/", import.meta.url)),
+			constants: fileURLToPath(new URL("./src/constants/", import.meta.url))
+		}
+	}
 })
