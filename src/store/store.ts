59303f283028a09dd7679427a5e22f488911dc45
MikeVM97 <vivancomayta@gmail.com>
2023-11-01 16:01:21 -0500

    refactor directories

---
+++ src/store/store.ts
@@ -0,0 +1,42 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { usersApi } from "api/getUsers"
+import { setupListeners } from "@reduxjs/toolkit/dist/query"
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import userReducer from "reducers/userSlice"
+
+const persistConfig = {
+	key: "root",
+	storage
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
+export const store = configureStore({
+	reducer: {
+		user: persistedReducer,
+		[usersApi.reducerPath]: usersApi.reducer
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			}
+		}).concat(usersApi.middleware)
+})
+
+export const persistor = persistStore(store)
+
+setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
